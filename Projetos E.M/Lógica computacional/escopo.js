506c9dd25fc70579c2c7d39de35bc6455a455583
NicolySilvaDeCastro <nicoly_castro@estudante.sesisenai.org.br>
2023-06-30 08:16:14 -0300

    Atividades em sala

---
+++ Projetos E.M/Lógica computacional/escopo.js
@@ -0,0 +1,10 @@
+const varGlobal =  "Essa é uma variável Global"
+///Escopo: 
+function mostrarValor() {
+let varLocal = "Essa é uma variável Local"
+
+console.log(varGlobal)
+console.log(varLocal)
+}
+mostrarValor()
+///Camel Case
---
+++ Projetos E.M/Lógica computacional/index.js
@@ -0,0 +1,9 @@
+function comparaNumeros(n1, n2){
+n1 = parseInt (document.getElementById("primeiro").value)
+n2 = parseInt (document.getElementById("segundo").value)
+if (n1 > n2){
+    alert (n1)
+}else{
+    alert (n2)  
+}
+}
